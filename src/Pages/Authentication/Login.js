52489abef8728f04613fdbfcbc4e753c1b1eb0e5
Anshul Tamrakar <anshultamrakar487@gmail.com>
2023-07-05 22:45:08 +0530

    eye password

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -2,12 +2,14 @@ import React from 'react'
 import "./Login.css"
 import axios from 'axios'
 import { useState , useEffect  } from 'react'
+import {AiFillEye , AiFillEyeInvisible} from "react-icons/ai"
 import {Link , useNavigate} from "react-router-dom"
 
 const Login = () => {
     const navigate = useNavigate();
     const [username , setUsername] = useState("")
     const [password , setPassword] = useState("")
+    const [showPassword , setShowPassword] = useState("password")
  
     const loginHandler = async(e) => {
       e.preventDefault()
@@ -36,7 +38,11 @@ const Login = () => {
         <label>Username :</label>
         <input type = "text" placeholder='Enter username' autoComplete='off' value = {username} onChange={(e) => setUsername(e.target.value)} autoFocus/>
         <label>Password : </label>
-        <input type = "password" placeholder = "Enter your password" value = {password} onChange={(e) => setPassword(e.target.value)}/>
+        <div className='password'>
+        <input type = {showPassword} placeholder = "Enter your password" value = {password} onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={() => setShowPassword("text")}><AiFillEye/></button>
+        </div>
+        
         <button type='submit' autoComplete='off'  value = {password} onChange={(e) => setPassword(e.target.value)}>Login </button>
         <button onClick={handleGuestLogin}>Guest login</button>
         <p>Need an account ? </p>
